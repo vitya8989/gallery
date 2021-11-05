e0f0a2bab6301c344cc4de2e5bd6e8d9e890b45d
vitya8989 <vitya898989@gmail.com>
2021-11-05 19:21:33 +0300

    event

--- js/event.js
+++ js/event.js
@@ -39,100 +39,6 @@ function unlockPadding() {
 	body.classList.remove('scroll-lock');
 	wrapper.style.paddingRight = "0";
 };
-let gallerySlider = new Swiper('.gallery-slider', {
-	slidesPerView: 1,
-	navigation: {
-		nextEl: '.gallery-slider__btn-next',
-		prevEl: '.gallery-slider__btn-prev'
-	},
-	pagination: {
-		el: '.gallery-slider__pagination',
-		type: 'fraction',
-	},
-	autoHeight: true,
-});
-
-gallerySlider.on('slideChange', function () {
-	addNull();
-});
-
-reinitSwiper(gallerySlider);
-
-
-;
-let mainEventsSlider = new Swiper('.main-events-slider', {
-	slidesPerView: 1,
-	spaceBetween: 20,
-	navigation: {
-		nextEl: '.main-events-slider__btn-next',
-		prevEl: '.main-events-slider__btn-prev'
-	},
-	pagination: {
-		el: '.main-events-slider__pagination',
-		type: 'fraction',
-	},
-	autoHeight: true,
-});
-
-mainEventsSlider.on('slideChange', function () {
-	addNull();
-});
-
-reinitSwiper(mainEventsSlider);
-;
-let aboutSlider = new Swiper('.about-slider', {
-	slidesPerView: 1,
-	navigation: {
-		nextEl: '.about-slider__btn-next',
-		prevEl: '.about-slider__btn-prev'
-	},
-	pagination: {
-		el: '.about-slider__pagination',
-		type: 'fraction',
-	},
-});
-
-aboutSlider.on('slideChange', function () {
-	addNull();
-});
-
-reinitSwiper(aboutSlider);
-;
-let howFindTitle = document.querySelector('.how-find__title');
-let howFindContent = document.querySelector('.how-find__content');
-let howFindBody = document.querySelector('.how-find__body');
-
-if (window.innerWidth < 1051) {
-	howFindContent.append(howFindTitle);
-}
-
-window.onresize = function () {
-	if (window.innerWidth < 1051) {
-		howFindContent.append(howFindTitle);
-	} else {
-		howFindBody.prepend(howFindTitle);
-	}
-}
-
-let scriptMap = document.createElement('script');
-scriptMap.src = 'https://api-maps.yandex.ru/2.1/?apikey=dd0f5e76-e8bb-42be-b558-f7af3b491cd2&lang=ru_RU';
-setTimeout(() => document.head.append(scriptMap), 1000);
-scriptMap.onload = function () {
-	ymaps.ready(init);
-};
-
-function init() {
-	var myMap = new ymaps.Map("map", {
-		center: [59.920163, 30.346132],
-		zoom: 17
-	});
-	var placemark = new ymaps.Placemark([59.920163, 30.346132], {
-		iconColor: '#FF0000',
-		iconCaption: 'Социалистическая ул., 21'
-	}
-	);
-	myMap.geoObjects.add(placemark);
-};
 let footerContacts = document.querySelector('.footer-contacts');
 let footerMenu = document.querySelector('.footer-menu');
 let footerTopContent = document.querySelector('.footer-top__content');
@@ -150,6 +56,22 @@ window.onresize = function () {
 	}
 }
 ;
+
+let eventSlider = new Swiper('.event-slider', {
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.event-slider__btn-next',
+		prevEl: '.event-slider__btn-prev'
+	},
+	pagination: {
+		el: '.event-slider__pagination',
+		type: 'fraction',
+	},
+});
+
+eventSlider.on('slideChange', function () {
+	addNull();
+});
 
 function addNull() {
 	let swiperPaginationCurrent = document.querySelectorAll('.swiper-pagination-current');
@@ -171,4 +93,6 @@ function reinitSwiper(swiper) {
 		swiper.update();
 	}, 200);
 }
+
+reinitSwiper(eventSlider);
 
